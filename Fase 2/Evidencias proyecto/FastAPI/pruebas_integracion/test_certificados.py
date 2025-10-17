a30d39e0a5f1776d47795ca811d24da5636696cc
Sebastian Palma <57689355+D4MNN665@users.noreply.github.com>
2025-10-17 18:06:56 -0300

    Se agregan pruebas de integracion
    regla de negocio: que un vecino pueda inscribirse a una juta siempre cuando sea mayor a 14 años

--- Fase 2/Evidencias proyecto/FastAPI/pruebas_integracion/test_certificados.py
+++ Fase 2/Evidencias proyecto/FastAPI/pruebas_integracion/test_certificados.py
@@ -1,35 +1,51 @@
 import sys
 import os
+import random
 
+from datetime import date, timedelta
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # Agrega la carpeta FastAPI al sys.path
 
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
+# PRUEBAS DE INTEGRACION SEGUN EL REQUERIMIENTO QUE UN VECINO MAYOR DE 14 AÑOS PUEDE INSCRIBIRSE
+def test_inscripcion_vecino_mayor_14():
+    rut = f"{random.randint(10000000, 99999999)}-0"
+    print(rut)
+    correo = f"{rut}@gmail.com"
+    fecha_nacimiento = int((date.today() - timedelta(days=15*365)).strftime("%Y%m%d")) # Calcula la fecha actual pero 15 años atrás
+    response = client.post("/vecinos/", json={
+        "nombre": "test",
+        "apellido": "mayor_14",
+        "rut": rut,
+        "correo": correo,
+        "fecha_nacimiento": fecha_nacimiento,
+        "numero_telefono": "912345678",
+        "direccion": "Calle falsa 123",
+        "contrasena": "123",
+        "miembro": True
+    })
+    print(response.status_code, response.json())
+    assert response.status_code == 200 or response.status_code == 201
 
-def test_emitir_certificado_campos_obligatorios():
-    # Falta el campo 'rut'
-    certificado_incompleto = {
-        "nombreVecino": "Benjamin Valenzuela",
-        "nacionalidad": "Chilena",
-        "domicilio": "av. rinconada 262",
-        "tipo_residencia": "Propietario",
-        "motivo": "Trámite",
-        "id_vecino": 1
-    }
-    response = client.post("/certificados/residencia", json=certificado_incompleto)
-    assert response.status_code == 422  # Unprocessable Entity
+# PRUEBAS DE INTEGRACION SEGUN EL REQUERIMIENTO QUE UN VECINO MENOR DE 14 AÑOS NO PUEDE INSCRIBIRSE
+def test_inscripcion_vecino_menor_14():
+    v_rut = f"{random.randint(10000000, 99999999)}-0"
+    print(v_rut)
+    correo = f"{v_rut}@gmail.com"
 
-
-    # Esta prueba verifica que la API responde con un error 422 cuando faltan campos obligatorios lo cual es cierto
-
-def test_actualizar_estado_certificado_campos_obligatorios():
-    # Falta el campo 'estado'
-    estado_incompleto = {
-        "razon": "Falta documentación"
-    }
-    response = client.put("/certificados/residencia/1", json=estado_incompleto)
-    assert response.status_code == 422  # Unprocessable Entity
-
-    # Esta prueba verifica que la API responde con un error 422 cuando faltan campos obligatorios lo cual es cierto
+    fecha_nacimiento = int((date.today() - timedelta(days=13*365)).strftime("%Y%m%d")) # Calcula la fecha actual pero 13 años atrás
+    response = client.post("/vecinos/", json={
+        "nombre": "test",
+        "apellido": "menor_14",
+        "rut": v_rut,
+        "correo": correo,
+        "fecha_nacimiento": fecha_nacimiento,
+        "numero_telefono": "912345679",
+        "direccion": "Calle falsa 123",
+        "contrasena": "123",
+        "miembro": True
+    })
+    print(response.status_code, response.json())
+    assert response.status_code == 400 or response.status_code == 422
