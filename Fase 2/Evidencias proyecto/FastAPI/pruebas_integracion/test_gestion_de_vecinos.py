ca455cfe1db45d059f068e26f5a51227f422f18d
Sebastian Palma <57689355+D4MNN665@users.noreply.github.com>
2025-10-23 17:03:13 -0300

    se agregan parametros en las pruebas
    se agregan parametros para que sea mas facil ejecutar las pruebas

--- Fase 2/Evidencias proyecto/FastAPI/pruebas_integracion/test_gestion_de_vecinos.py
+++ Fase 2/Evidencias proyecto/FastAPI/pruebas_integracion/test_gestion_de_vecinos.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from urllib import response
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from main import app
 from fastapi.testclient import TestClient
@@ -8,22 +9,30 @@ client = TestClient(app)
 
 def obtener_token(rut, contrasena):
     response = client.post("/login", json={"rut": rut, "contrasena": contrasena})
-    assert response.status_code == 200
+    assert response.status_code == 200 # OK
+    #print("Status code:", response.status_code)
+    #print("Response text:", response.text)
     return response.json()["access_token"]
 
 def test_admin_puede_modificar_datos_sensibles():
     token = obtener_token("20.722.122-8", "147") # Credenciales de admin
     headers = {"Authorization": f"Bearer {token}"}
-    response = client.put("/vecinos/68", headers=headers, json={
-        "correo": "nuevo_correo@example.com",
+    id_vecino = 88 
+    response = client.put(f"/vecinos/{id_vecino}", headers=headers, json={
+        "correo": "nuevo_correo@gmail.com",
     })
-    assert response.status_code == 200
-    assert response.json()["correo"] == "nuevo_correo@example.com"
+    assert response.status_code == 200 # OK
+    #print("Status code:", response.status_code)
+    #print("Response text:", response.text)
+    assert response.json()["correo"] == "nuevo_correo@gmail.com"
 
 def test_vecino_no_puede_modificar_datos_sensibles():
-    token = obtener_token("20.820.262-6", "123")  # Usa credenciales reales
+    token = obtener_token("20.820.262-6", "123")  # Credenciales de vecino
     headers = {"Authorization": f"Bearer {token}"}
-    response = client.put("/vecinos/69", headers=headers, json={
-        "correo": "hackeo@example.com",
+    id_vecino = 88 
+    response = client.put(f"/vecinos/{id_vecino}", headers=headers, json={
+        "correo": "sebastianpalma@gmail.com",
     })
     assert response.status_code == 403  # Forbidden
+    #print("Status code:", response.status_code)
+    #print("Response text:", response.text)
