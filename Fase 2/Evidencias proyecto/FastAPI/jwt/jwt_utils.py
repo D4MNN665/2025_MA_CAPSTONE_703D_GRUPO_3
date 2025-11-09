c5e02dd9606bf9c889b069b0374e3a78980588af
Sebas <57689355+D4MNN665@users.noreply.github.com>
2025-11-08 23:30:48 -0300

    Se agrega OAuth2 scheme

--- Fase 2/Evidencias proyecto/FastAPI/jwt/jwt_utils.py
+++ Fase 2/Evidencias proyecto/FastAPI/jwt/jwt_utils.py
@@ -1,11 +1,14 @@
 from jose import jwt, JWTError, ExpiredSignatureError
+from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta, timezone
 import os 
-from fastapi import HTTPException
+from fastapi import HTTPException, status, Depends
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")  # Ajusta el endpoint si es diferente
 
 def crear_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
@@ -26,3 +29,12 @@ def verificar_access_token(token: str):
         raise HTTPException(status_code=401, detail="Token inválido")
     except Exception as e: 
         raise HTTPException(status_code=500, detail=f"Error al verificar el token: {str(e)}")
+
+def Obtener_usuario_actual_JWT(token: str = Depends(oauth2_scheme)):
+    payload = verificar_access_token(token)
+    if not payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="No se pudo validar las credenciales",
+        )
+    return payload
